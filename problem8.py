662807afe6732d2d57d5f5ee4cd672e0cd402387
Joe Pinsonault <joe.pinsonault@gmail.com>
2014-03-30 23:31:14 -0700

    Added problems

--- problem8.py
+++ problem8.py
@@ -8,9 +8,9 @@ from itertools import permutations
 
 from utils import *
 
-genome = "AGGGGAGGAAGGGACGGACGGGGAGGGAGGGAAGGCTAGGGAGGAGACGGGAGACGAGGCTAGGGAGGGAGGGAGGAGACGAGGGGAAGGAGGGGACTACTAGGGAGACGGGGAAGGGACGGGGACTACTACTAAGGCTACTAGGGACTACTAGGAGGGAAGGGGAAGGGACGGGGAGGACTAAGGGACGAGGCTAGACGCTAGGAGGAGGGAAGGAGGGGAGACGGGAAGGCTAGGAGGAGACGCTAGGAGACGAGGCTAGACGGACGGGGAGACGCTAGGAGACGCTAGACGGGGAGGACTAGGGAAGGCTAGACGGGACTAGGACTAGGACTA"
+genome = "CACGTACAACACGTCGTCACGTCGTCGTCGTCACACACAACACGTACATTACATTCACGTCACGTTTTTCATTCACACGTTTCACATTCATTCACGTTTACATTTTTTTTACATTTTCACGTCAACATTCACGTACATTTTCACACATTCAACACGTTTCACGTCACACACGTCATTCACAACATTCATTCGTCAACACATTCGTACACGTCACACACACGTACACACAACACACGTCACACA"
 k = 9
-d = 3
+d = 2
 
 def main():
     kmers = find_kmers(k, genome)
@@ -19,9 +19,10 @@ def main():
 
     for count, search_string in enumerate(kmers.keys()):
         if count % (num_kmers/20) == 0:
-            print("{:.2f}".format(count/float(num_kmers)*100))
+            print("{:.2f}%".format(count/float(num_kmers)*100))
 
         permuted_strings = mutations(search_string, d)
+        # permuted_strings += mutations(reverse_compliment(search_string), d)
         # permuted_strings = permutations(search_string)
         # num_permutations = len(permuted_strings)
         # print(num_permutations)
@@ -31,18 +32,26 @@ def main():
                 # print("{:.2f}".format(pcount/float(num_permutations)*100))
             if permutation not in fuzzy_kmers:
                 # print(permutation)
+                reversed_permuation = reverse_compliment(permutation)
+                reverse_not_equal = reversed_permuation != permutation
 
                 for index in range(len(genome) - k + 1):
                     word = genome[index:index + k]
                     miss_count = 0
+                    reversed_miss_count = 0
                     for x, letter in enumerate(word):
                         if letter != permutation[x]:
                             miss_count += 1
-                            if miss_count > d:
-                                break
+
+                        if letter != reversed_permuation[x]:
+                            reversed_miss_count += 1
 
                     if miss_count <= d:
                         fuzzy_kmers[permutation] += 1
+
+                    if reverse_not_equal:
+                        if reversed_miss_count <= d:
+                            fuzzy_kmers[permutation] += 1
 
 
     sorted_kmers = sorted(fuzzy_kmers.iteritems(), key=operator.itemgetter(1))
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from pprint import pprint
 import re
+import itertools
 
 
 def find_kmers(k, genome):
@@ -16,3 +17,30 @@ def find_kmers(k, genome):
 def read_file(filename):
     with open(filename) as o_file:
         return o_file.readlines()[0].strip()
+
+
+def hamming_distance(s1, s2):
+    #Return the Hamming distance between equal-length sequences
+    if len(s1) != len(s2):
+        raise ValueError("Undefined for sequences of unequal length")
+    return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+
+
+def mutations(word, hamming_distance, charset='ATCG'):
+    for indices in itertools.combinations(range(len(word)), hamming_distance):
+        for replacements in itertools.product(charset, repeat=hamming_distance):
+            mutation = list(word)
+            for index, replacement in zip(indices, replacements):
+                mutation[index] = replacement
+            yield "".join(mutation)
+
+
+def reverse_compliment(string):
+    letter_map = {
+        "A": "T",
+        "G": "C",
+        "T": "A",
+        "C": "G",
+    }
+
+    return "".join(reversed([letter_map[letter] for letter in string]))
